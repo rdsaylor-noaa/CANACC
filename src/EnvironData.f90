9e971066c867cecd720f26fa133363a4c6131b60
Rick Saylor <rdsaylor@loggerhead.local>
2021-08-05 13:40:48 -0400

    simplify IC for qh

--- src/EnvironData.f90
+++ src/EnvironData.f90
@@ -140,7 +140,6 @@ contains
 
        ! Initial guess for qh based on rhzref and tk
        qh(n)        = SpecificHumidity(rhzref, tk(npts), pmb(npts))
-!      qh(n)        = SpecificHumidity(rhzref, tk(n), pmb(n))
 
        ! Calculate mean wind speed (Meyers et al., 1998)
        ubar(n)      = CalcMeanWindSpeed(z(n), ubzref)
